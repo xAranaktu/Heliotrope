beec5d18fba025419db95c99bb5451c73e85a781
xAranaktu <paweh10@gmail.com>
2019-04-21 20:44:10 +0200

    fix make_backup.py

--- make_backup.py
+++ make_backup.py
@@ -4,4 +4,7 @@ import os
 
 today = datetime.datetime.today()
 
-copyfile('loot.json', os.path.join('backup', 'loot_{}_{}_{}.json'.format(today.day, today.month, today.year)))
+copyfile(
+    os.path.join('data', 'loot.json'), 
+    os.path.join('backup', 'loot_{}_{}_{}.json'.format(today.day, today.month, today.year))
+)
